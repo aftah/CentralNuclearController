32cdbd9da87411615b42c0fb86913e063a4652d0
aftah <abdelfetah.hamra@skynet.be>
2019-09-11 17:26:38 +0200

    Potetioometre

--- Assets/InputPortSerie.cs
+++ Assets/InputPortSerie.cs
@@ -1,34 +1,33 @@
 ﻿using System;
 using System.IO.Ports;
+using System.Threading;
 using UnityEngine;
 
 
 
 public class InputPortSerie : MonoBehaviour
 {
-    [SerializeField]
-    private static string COMPort = "COM4"; //Define the Comport Unity should listen to
+
+    public static string COMPort = "COM4"; //Define the Comport Unity should listen to
     [SerializeField]
     private bool portOpen = false; //If the port is open this boolean is true
 
     private SerialPort serialPort = new SerialPort(COMPort, 9600);
 
-   
+
+
     [SerializeField]
     private string incomingLineData = ""; //The Incoming data will be stored in this variable
-   
 
 
+    //Event
     public event EventHandler<OnButtonJumpEventArgs> onButtonJumpEvent;
+    public event EventHandler<OnSlideEventArgs> onSlideEvent;
+
+
+
 
    
-
-    private void OnButtonJumpEvent(OnButtonJumpEventArgs e)
-    {
-        onButtonJumpEvent?.Invoke(this, e); 
-    }
-
-
 
     // Use this for initialization
     void Start()
@@ -36,7 +35,9 @@ public class InputPortSerie : MonoBehaviour
         OpenConnection(); // Open the connection
         if (serialPort.IsOpen)
         {
+
             Debug.Log("I'm open");
+            
         }
         else
         {
@@ -47,30 +48,52 @@ public class InputPortSerie : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       // updateSlower++; //Recieve data every 4 frames
-        if (portOpen && serialPort.IsOpen) //&& updateSlower >= updateInterval
-        { //
-             
-            RecieveInput();
-           // updateSlower = 0;
+
+        if (portOpen && serialPort.IsOpen)
+        {
+
+            // RecieveInput();
+            ParseIncomingData(incomingLineData);
+
         }
     }
 
+    //invoke event jump
+    private void OnButtonJumpEvent(OnButtonJumpEventArgs e)
+    {
+        onButtonJumpEvent?.Invoke(this, e);
+    }
+
+    //invoke event Slide
+    private void OnSlideEvent(OnSlideEventArgs e)
+    {
+
+        onSlideEvent?.Invoke(this, e);
+    }
+
+    //Connection serial port
     public void OpenConnection()
     {
         if (serialPort != null)
-        { 
+        {
+            Thread sampleThread = new Thread(new ThreadStart(RecieveInput));
+            sampleThread.IsBackground = true;
+
             if (serialPort.IsOpen)
-            { 
+            {
                 serialPort.Close();
+                
 
                 Debug.Log("Closing port as it was already open");
             }
             else
             {
-                serialPort.Open(); 
-                serialPort.ReadTimeout = 1; 
+                serialPort.Open();
+                serialPort.ReadTimeout = 100;
                 portOpen = true;
+               
+                // start thread
+                sampleThread.Start();
 
             }
         }
@@ -89,47 +112,94 @@ public class InputPortSerie : MonoBehaviour
 
     void RecieveInput()
     {
-        try
+        while (true)
         {
-            
-
-           incomingLineData = serialPort.ReadLine();
-          //  int jumpData = int.Parse(incomingLineData);
-           
-            //test button jump
-            JumpPlayer(incomingLineData);
+            if (serialPort.IsOpen)
+            {
 
 
-            //serialPort.DataReceived += (s, e) =>
-            //{
-            //    var c = serialPort.ReadExisting();
-            //   Debug.Log(c);
-            //};
+                try
+                {
 
-           
-          
+
+                    incomingLineData = serialPort.ReadLine();
+                     ParseIncomingData(incomingLineData);
+
+
+
+                }
+                catch (Exception errorpiece)
+                {
+
+                    Debug.Log("Error 1: " + errorpiece);
+
+                }
+
+            }
+            else
+            {
+
+                //something to do for the thread
+            }
         }
-        catch (Exception errorpiece)
-        {
-           
-                Debug.Log("Error 1: " + errorpiece);
-           
-        }
-
-
     }
 
-    private void JumpPlayer(string jumpData)
+    private void ParseIncomingData(string incomingLineData)
     {
-        if (jumpData == "SPACE")
+        string[] buffer;
+        string[] tempBuffer;
+        int valButton;
+        bool is_Jump;
+        int valSlider;
+        bool is_Sliding;
+
+        if (incomingLineData.Length > 0)
         {
-            OnButtonJumpEvent(new OnButtonJumpEventArgs() { Jump = true });
+            buffer = incomingLineData.Split(',');
+
+                      
+            tempBuffer = buffer[0].Split('=');
+            is_Jump = int.TryParse(tempBuffer[1], out valButton);
+            Debug.Log(valButton);
+
+            tempBuffer = buffer[1].Split('=');
+            is_Sliding = int.TryParse(tempBuffer[1], out valSlider);
+            Debug.Log(valSlider);
+
+            if (is_Jump) //jump
+            {
+                JumpPlayer(valButton);
+            }
+
+            if (is_Sliding) //move
+                MoveSlide(valSlider);
+
         }
     }
 
-    public class OnButtonJumpEventArgs : EventArgs 
+    private void MoveSlide(int valSlider)
+    {
+        if (valSlider >= 0)
+        {
+            OnSlideEvent(new OnSlideEventArgs() { ValueSlide = valSlider }); 
+        }
+    }
+
+    //Jump
+    private void JumpPlayer(int jump)
+    {
+        if (jump == 1)
+            OnButtonJumpEvent(new OnButtonJumpEventArgs() { Jump = true });
+
+    }
+
+    public class OnButtonJumpEventArgs : EventArgs
     {
         public bool Jump { get; set; }
     }
 }
 
+public class OnSlideEventArgs
+{
+    public int ValueSlide { get; set; }
+}
---
+++ Assets/SlideBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlideBar : MonoBehaviour
+{
+    public Slider pressionBar;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        FindObjectOfType<InputPortSerie>().onSlideEvent += SlideBar_onSlideEvent; 
+    }
+
+    public void SlideBar_onSlideEvent(object sender, OnSlideEventArgs e)
+    {
+        pressionBar.value = (float)e.ValueSlide;
+        //Debug.Log("Potentiometre = " + e.ValueSlide);
+    }
+
+    void Start()
+    {
+        pressionBar = GetComponent<Slider>();
+    }
+
+   
+}
